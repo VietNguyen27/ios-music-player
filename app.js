4c67a9233d78896126d163b13041be7ed96c7703
VietNguyen27 <nquocviet2307@gmail.com>
2021-08-20 14:02:17 +0700

    fix crash

--- app.js
+++ app.js
@@ -13,4 +13,7 @@ app.use('/musics', musicRoutes);
 app.use('/songs', express.static(path.join(__dirnamePath, '/songs')));
 app.use('/img', express.static(path.join(__dirnamePath, '/img')));
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(
+  process.env.PORT || 5000,
+  console.log(`Server running on port ${PORT}`)
+);
